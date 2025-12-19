d8a4e299e0649d60799af42c20fd021f49e6826c
prem yadav <yprem1568@gmail.com>
2025-12-19 19:17:00 +0530

    Added new topic to chp11(Collections).That new topic is stack.

---
+++ chp11/src/queue/Main.java
@@ -0,0 +1,22 @@
+package queue;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Main {
+	public static void main(String[] args) {
+		
+		Queue<String> queue = new LinkedList<String>();
+		
+		queue.add("Prem");
+		queue.add("Yash");
+		queue.add("Sumit");
+		queue.add("vibha");
+		System.out.println(queue);
+		System.out.println(queue.peek());
+		System.out.println(queue.element());
+		queue.remove();
+		System.out.println(queue);
+	}
+
+}
