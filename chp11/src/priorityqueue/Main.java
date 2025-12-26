b3887070023bade48cfee8de59f68f5baad699b9
prem yadav <yprem1568@gmail.com>
2025-12-26 21:33:57 +0530

    Priority Queue

---
+++ chp11/src/priorityqueue/Main.java
@@ -0,0 +1,29 @@
+package priorityqueue;
+
+import java.util.PriorityQueue;
+import java.util.Stack;
+
+
+public class Main {
+	public static void main(String[] args) {
+		PriorityQueue<Integer> pq = new PriorityQueue<Integer>();
+		pq.add(100);
+		pq.offer(20);
+		pq.add(700);
+		pq.add(56);
+		pq.add(920);
+		pq.add(290);
+		System.out.println(pq);
+		
+		
+		 //Find the smallest number and print in ascending order
+		while (!pq.isEmpty()) {
+			System.out.println(pq.poll());
+			
+		}
+	}
+	
+	
+	
+
+}
