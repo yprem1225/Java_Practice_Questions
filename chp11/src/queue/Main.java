56c9a7761f6f542515d404cdd200022453114fbe
prem yadav <yprem1568@gmail.com>
2025-12-26 21:01:24 +0530

    queue

--- chp11/src/queue/Main.java
+++ chp11/src/queue/Main.java
@@ -1,8 +1,10 @@
 
 package queue;
 
+import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.Stack;
 
 public class Main {
 	public static void main(String[] args) {
@@ -13,13 +15,37 @@ public class Main {
 		queue.add("Yash");
 		queue.add("Sumit");
 		queue.add("vibha");
-		System.out.println(queue);
-		System.out.println(queue.peek());
-		System.out.println(queue.element());
-		queue.remove();
-		System.out.println(queue);
-		System.out.println(queue.poll());
+//		System.out.println(queue);
+//		System.out.println(queue.peek());
+//		System.out.println(queue.element());
+//		queue.remove();
+//		System.out.println(queue);
+//		System.out.println(queue.poll());
 		
+		Iterator<String> it = queue.iterator();
+		//Print queue in order using iterator
+//		while (it.hasNext()) {
+//			String name=  it.next();
+//			System.out.println(name);
+//			
+//		}
+//		// Print queue in order using foreach loop
+//		for (String name : queue) {
+//			System.out.println(name);
+//			
+//		}
+		
+		//Print in reverse order
+		Stack<String> stack = new Stack<String>(); //Move queue -> stack
+		while (!queue.isEmpty()) {
+			stack.push(queue.poll());
+		}
+		
+		while (!stack.isEmpty()) { //move stack -> queue
+			queue.add(stack.pop());
+			
+		}
+		System.out.println(queue);
 	}
 
 }
